8b232c6b56d53d8a6abb831235ec5a118bc3e12c
Nathan Buskirk <nbuskirk@gmail.com>
2016-06-19 12:19:10 -0700

    React Rainbow Redux

---
+++ readme.md
@@ -0,0 +1,13 @@
+<h1>React Rainbow Redux!</h1>
+
+<p>
+Version 2 of the React Rainbow Challenge. This version consists of 2 components. Rainbow, and RainbowRow. Rainbow will draw RainbowRow's from data passed into the Rainbow as a 'data' property. Eventually this should be probably fed from a source, and acquired via a GET.
+</p>
+
+<p>
+2 'states' can be manipulated here. Each row has a state for whether it's clicked(highlighted) or not, and the options box will call a re-render of Rainbow using the boolean value from itself to set the state on Rainbow triggering a re-render with double rows (doubles the data source to draw from).
+</p>
+
+<small>
+Nathan Buskirk &copy; 2016
+</small>
---
+++ src/rainbow.js
@@ -0,0 +1,76 @@
+var Rainbow = React.createClass({
+	getInitialState:function() {
+		return {
+			doubleRainbow: this.props.doubleRainbow || false
+		}
+	},
+	componentDidUpdate: function() {
+		console.log('Rainbow Update Called!');
+	},
+	drawRainbow:function() {	
+		var doubleRainbow = this.state.doubleRainbow;
+		var colors = this.props.data;
+		if(doubleRainbow){
+			colors = colors.concat(colors);
+		}
+		return colors.map(function(color,i){
+			return <RainbowRow color={color.hex} key={i} />
+		})
+	},
+	changeState:function(props){
+		this.setState({
+			doubleRainbow: !this.state.doubleRainbow
+		}, function() {
+			console.log("Double Rainbow State updated to: "+this.state.doubleRainbow);
+		})
+	},
+	drawOptions:function() {
+		return (
+			<div className="options">
+				<input type="checkbox" checked={this.state.isChecked} onChange={this.changeState} />Double Rainbow
+			</div>
+		)
+	},
+	render: function() {
+		var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+		return (
+			<div className='container' >
+			<ReactCSSTransitionGroup transitionName="rainbowRow" transitionAppear={true} transitionAppearTimeout={500} transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+				{ this.drawRainbow() }
+				{ this.drawOptions() }
+			</ReactCSSTransitionGroup>
+			</div>
+		)
+	}
+})
+var RainbowRow = React.createClass({
+	getInitialState: function(){
+		return {
+			isSelected: this.props.isSelected || false
+		}
+	},
+	componentDidUpdate: function() {
+		console.log('Rainbow Row Update Called!');
+	},
+	handleClick:function() {
+		this.setState({
+			isSelected: !this.state.isSelected
+		},function(){ 
+			console.log('Rainbow Row state updated to: '+this.state.isSelected);
+		})
+	},
+	render: function() {
+		var highlightColor = "#FFFFFF"
+		var style = {
+			backgroundColor: this.props.color,
+			padding: '0 25px',
+			display: 'inline-block',
+			height: window.innerHeight+"px"
+		};
+		if(this.state.isSelected) {
+			style.backgroundColor = highlightColor;
+		}
+		return <a onClick={this.handleClick} href="#" color={this.state.isSelected} style={style}></a>
+	}
+})
+ReactDOM.render(<Rainbow data={colorData}  />, document.getElementById('rainbow'));
